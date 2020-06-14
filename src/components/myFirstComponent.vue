fd62e58d20d34cf3081f67e58298556267edfbaf
Giorgos Kal <kalpgiorgos@gmail.com>
2020-06-14 23:48:05 +0300

    add Vue Formulate

--- src/components/myFirstComponent.vue
+++ src/components/myFirstComponent.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <p>{{ msg }}</p>
+    <FormulateInput type="text" label="What is your name?" v-model="value"/>
+    <strong>
+      Hello
+      <em>{{ value }}</em>!
+    </strong>
   </div>
 </template>
+
 
 <script>
 export default {
   name: "myName",
   props: {
-    msg: String
+    value: "My initial value"
   }
 };
 </script>
 
 <style>
-
 </style>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <myName msg="My name is George"/>
+    <signUp/>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import myName from "@/components/myFirstComponent.vue";
+import signUp from "@/components/signup.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    myName
+    myName,
+    signUp
   }
 };
 </script>
